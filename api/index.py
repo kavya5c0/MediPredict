23983186662432397f0207c6b8c3a0e53abaa390
Kavyasri <tanirukavyasri@gmail.com>
2026-09-20 00:37:31 +0530

    Simplify handler function

--- api/index.py
+++ api/index.py
@@ -1,9 +1,5 @@
-# Vercel serverless function
 def handler(request):
     return {
-        "statusCode": 200,
-        "body": '{"message": "Hello from Vercel"}',
-        "headers": {
-            "Content-Type": "application/json"
-        }
+        "body": "Hello from Vercel!",
+        "statusCode": 200
     }
